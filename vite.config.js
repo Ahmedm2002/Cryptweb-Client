239e465ce373c1adf2a835ce0e8e9a658d267be0
Ahmed Mujtaba <ahmedmujtaba0159@gmail.com>
2026-07-20 16:41:22 +0500

    error 306 fixed

--- vite.config.js
+++ vite.config.js
@@ -9,14 +9,5 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: 300,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules/react-dom") || id.includes("node_modules/react/") || id.includes("node_modules/react-router")) return "vendor";
-          if (id.includes("node_modules/phosphor-react")) return "phosphor";
-          if (id.includes("node_modules/socket.io")) return "socket";
-        },
-      },
-    },
   },
 });
